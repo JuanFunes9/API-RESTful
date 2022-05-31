5f12f47abd8849ee41722058d72c95f1a85ea0e3
juanf <juan.funes.96@outlook.com>
2022-05-30 23:10:48 -0300

    se hizo hasta el punto 2

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
 //Imports
 const express = require( 'express' );
+const router = express.Router();
+const fs = require( 'fs' );
+
+
 const app = express();
 
 //Settings
 const PORT = process.env.PORT || 8080;
 
 //Routes
-app.get( '/', ( req, res ) => {
-    res.send( 'hola' );
+//1) Devuelve todos los productos
+router.get( '/', ( req, res ) => {
+    const read = fs.readFileSync( './productos.txt', 'utf-8' );
+    const products = JSON.parse( read );
+    res.json( products );
 });
 
+//2) Devuelve un producto segun su id
+router.get( '/:id', ( req, res ) => {
+    const id = Number( req.params.id );
+    const read = fs.readFileSync( './productos.txt', 'utf-8' );
+    const products = JSON.parse( read );
 
+    const product = products.find( prod => prod.id === id );
+    res.json( product );
+});
+
+//3) Recibe y agrega un producto. Devuelve el producto agregado y su ID asignada:
+router.post( '/', ( req, res ) => {
+    const read = fs.readFileSync( './productos.txt', 'utf-8' );
+    const products = JSON.parse( read );
+    
+    res.json( product );
+});
+
+//----
+app.use( '/api/productos', router );
 //Server listening
 const server = app.listen( PORT, () => {
     console.log( `Server on PORT: ${ PORT }` );
